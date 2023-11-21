68028e60f176273dc5bbed0bf3c12176800fe3c8
hafsaa05 <142868426+hafsaa05@users.noreply.github.com>
2023-11-21 10:40:21 +0500

    Update Q6.c

--- Lab11/23K-0064/Q6.c
+++ Lab11/23K-0064/Q6.c
@@ -5,15 +5,11 @@ date in the structure. Now add 45 days to the current date and display the final
 */
 #include <stdio.h>
 #include <time.h>
-
-// Define a structure for date
 struct Date {
     int day;
     int month;
     int year;
 };
-
-// Function to add days to a date
 void addDays(struct Date *date, int daysToAdd) {
     // Convert the date to time_t
     struct tm timeStruct = {
@@ -22,13 +18,10 @@ void addDays(struct Date *date, int daysToAdd) {
         .tm_year = date->year - 1900  // Adjust for years since 1900
     };
 
-    // Convert the structure to time_t
     time_t t = mktime(&timeStruct);
-
-    // Add the specified number of seconds (daysToAdd * 24 hours * 60 minutes * 60 seconds)
+    
     t += daysToAdd * 24 * 60 * 60;
-
-    // Convert the result back to a structure
+    
     struct tm *resultTime = localtime(&t);
 
     // Update the date structure
@@ -38,24 +31,19 @@ void addDays(struct Date *date, int daysToAdd) {
 }
 
 int main() {
-    // Get the current time
     time_t currentTime = time(NULL);
     struct tm *localTime = localtime(&currentTime);
 
-    // Create a structure for the current date
     struct Date currentDate = {
         .day = localTime->tm_mday,
         .month = localTime->tm_mon + 1,  // Adjust for 1-indexed months
         .year = localTime->tm_year + 1900  // Adjust for years since 1900
     };
 
-    // Display the current date
     printf("Current Date: %02d-%02d-%d\n", currentDate.day, currentDate.month, currentDate.year);
 
-    // Add 45 days to the current date
     addDays(&currentDate, 45);
 
-    // Display the final date
     printf("Final Date after adding 45 days: %02d-%02d-%d\n", currentDate.day, currentDate.month, currentDate.year);
 
     return 0;
