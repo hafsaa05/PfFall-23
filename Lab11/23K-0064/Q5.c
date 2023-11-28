595389b6642323f6b6adf30a37b396a3b08b3bd8
hafsaa05 <142868426+hafsaa05@users.noreply.github.com>
2023-11-28 09:14:31 +0500

    Delete Lab11/23K-0064/Q5.c

--- Lab11/23K-0064/Q5.c
+++
@@ -1,7 +0,0 @@
-/* Date: 11/21/2023
-Programmer: Hafsa Rashid
-Descrip:called Organization (Outer structure). The structure Organization has the data members like
-organisation_name,organisation_number. The Employee structure is nested inside the structure
-Organization and it has the data members like employee_id, name, salary.
-*/
-
