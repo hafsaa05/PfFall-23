1feb59ea9501c3c9b6f2787cd03d6422a8cad6ac
hafsaa05 <142868426+hafsaa05@users.noreply.github.com>
2023-11-28 09:14:51 +0500

    Delete Lab11/23K-0064/Q8.c

--- Lab11/23K-0064/Q8.c
+++
@@ -1,8 +0,0 @@
-/* Date: 11/21/2023
-Programmer: Hafsa Rashid
-Descrip: You are transporting some boxes through a tunnel, where each box is a parallelepiped, and is
-characterized by its length, width and height.
-The height of the tunnel is 41 feet, and the width can be assumed to be infinite. A box can be carried
-through the tunnel only if its height is strictly less than the tunnel's height. Find the volume of each box
-that can be successfully transported to the other end of the tunnel. Note: Boxes cannot be rotated.
-*/
